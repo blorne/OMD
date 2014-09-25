b7d8b52d9892d9415dcd66c6663fa330d649dc0b
PmL87 <martin@Martins-MacBook-Pro.local>
2014-09-25 21:23:10 +0200

    update
    tog bort onödiga ”this”

--- OMD/src/Program/Program.java
+++ OMD/src/Program/Program.java
@@ -6,8 +6,8 @@ import java.util.*;
 public abstract class Program extends ArrayList<Command> {
 	public String toString() {
 		StringBuilder str = new StringBuilder();
-		for(int i = 0; i < this.size(); i++)
-			str.append(i + " " + this.get(i) + "\n");
+		for(int i = 0; i < size(); i++)
+			str.append(i + " " + get(i) + "\n");
 		return str.toString();
 	}
 }
